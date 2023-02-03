d2984df8f39da4d5620aa73625f560fbd5978ca5
kien tran <kientv@dinoland.io>
2023-02-03 21:47:27 +0700

    connect mongoDB with nodejs

--- src/post/dto/post.dto.ts
+++ src/post/dto/post.dto.ts
@@ -1,10 +1,16 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreatePostDto {
+    @IsNotEmpty()
     content: string;
+    description: string;
     title: string;
 }
 
 export class UpdatePostDto {
+    @IsNotEmpty()
     id: number;
     content: string;
+    @IsNotEmpty()
     title: string;
 }
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { PostController } from './folder/post.controller';
+import { PostController } from './controller/post.controller';
+import { PostSchema } from './models/post.model';
 import { PostService } from './post.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostRepository } from './repositories/post.rebository';
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: 'Post',
+                schema: PostSchema,
+            },
+        ]),
+    ],
     controllers: [PostController],
-    providers: [PostService],
+    providers: [PostService, PostRepository],
 })
 export class PostModule { }
